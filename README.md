3b5737cc9898c399e57096acb39aacf696ebfbcf
bengreenier <begreen@microsoft.com>
2017-05-22 11:36:57 -0400

    v0.2.0

--- README.md
+++ README.md
@@ -30,6 +30,31 @@ Content-Length: 119
 {"username":"RottenAnt","availability":{"github":true,"dot-com":false,"twitter":true,"linkedin":false,"facebook":true}}
 ```
 
+or (supporting non-standard cors headers by request)
+
+```
+GET /http://usernames.io/new HTTP/1.1
+Host: localhost:3000
+X-Accept-Headers: X-Nonstandard-Header
+```
+
+=>
+
+```
+HTTP/1.1 200 OK
+Access-Control-Max-Age: 86400
+Access-Control-Allow-Origin: *
+Access-Control-Allow-Methods: POST,GET,PUT,DELETE,OPTIONS
+Access-Control-Allow-Headers: X-Requested-With,Access-Control-Allow-Origin,X-HTTP-Method-Override,Content-Type,Authorization,Accept,X-Nonstandard-Header
+Access-Control-Allow-Credentials: true
+Content-Type: application/json; charset=utf-8
+Date: Fri, 19 May 2017 21:06:53 GMT
+Connection: keep-alive
+Content-Length: 119
+
+{"username":"RottenAnt","availability":{"github":true,"dot-com":false,"twitter":true,"linkedin":false,"facebook":true}}
+```
+
 ## License
 
 MIT
--- index.js
+++ index.js
@@ -1,16 +1,49 @@
 const urlutils = require('url')
 const fetch = require('node-fetch')
-const microCors = require('micro-cors')
 const rateLimit = require('micro-ratelimit')
 
-const cors = microCors()
+const DEFAULT_ALLOW_METHODS = [
+  'POST',
+  'GET',
+  'PUT',
+  'PATCH',
+  'DELETE',
+  'OPTIONS'
+]
+
+const DEFAULT_ALLOW_HEADERS = [
+  'X-Requested-With',
+  'Access-Control-Allow-Origin',
+  'X-HTTP-Method-Override',
+  'Content-Type',
+  'Authorization',
+  'Accept'
+]
+
 const handler = async (req, res) => {
+    const corsHeaders = DEFAULT_ALLOW_HEADERS
+
+    if (req.headers['x-accept-headers']) {
+        corsHeaders.push(req.headers['x-accept-headers'].split(','))
+    }
+    
+    res.setHeader('Access-Control-Allow-Origin', '*')
+    res.setHeader('Access-Control-Max-Age', 60 * 60 * 24)
+    res.setHeader('Access-Control-Allow-Methods', DEFAULT_ALLOW_METHODS.join(','))
+    res.setHeader('Access-Control-Allow-Headers', corsHeaders.join(','))
+    res.setHeader('Access-Control-Allow-Credentials', 'true')
+
+    if (req.method === 'OPTIONS') {
+        return {}
+    }
+
     const url = urlutils.parse(req.url)
     const remote = url.path.substr(1)
     const remoteUrl = urlutils.parse(remote)
 
     const headers = req.headers
     headers['host'] = remoteUrl.host + (remoteUrl.port ? `:${remoteUrl.port}` : '')
+    delete headers['x-accept-headers']
 
     const response = await fetch(remoteUrl, {
         method: req.method,
@@ -21,4 +54,4 @@ const handler = async (req, res) => {
     res.end(await response.buffer())
 }
 
-module.exports = cors(rateLimit(handler))
+module.exports = rateLimit(handler)
